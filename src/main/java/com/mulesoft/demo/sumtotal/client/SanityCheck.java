b317e3961c262f80f77176f031a296350560087b
Tom Motley <tmotley@mulesoft.com>
2013-05-20 12:03:43 -0700

    amended wsdl2java client gen to use JAXWS-2.1 bindings

--- src/main/java/com/mulesoft/demo/sumtotal/client/SanityCheck.java
+++ src/main/java/com/mulesoft/demo/sumtotal/client/SanityCheck.java
@@ -1,11 +1,24 @@
 package com.mulesoft.demo.sumtotal.client;
 
+import com.mulesoft.demo.sumtotal.client.auth.*;
+
 /**
  *
  */
 public class SanityCheck {
 
     public static void main(String [] args) throws Exception {
-
+        UserCredentials creds = new UserCredentials();
+        creds.setUsername("wstest");
+        creds.setPasscode("learning");
+        creds.setAccountType(UserType.WEB_SERVICE);
+        creds.setAuthenticationType(UserAuthenticationType.NOT_SPECIFIED);
+        Authentication auth = new Authentication();
+        AuthenticationSoap soap = auth.getAuthenticationSoap12();
+        soap.login(creds);
+        GetLoggedInUser getLoggedInUser = new GetLoggedInUser();
+        getLoggedInUser.setPopulationLevel(1);
+        GetLoggedInUserResponse resp = soap.getLoggedInUser(getLoggedInUser);
+        System.out.println("LAST NAME OF LOGGED IN USER: " + resp.getGetLoggedInUserResult().getLastName());
     }
 }
